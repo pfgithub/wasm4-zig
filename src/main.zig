047092598cc32cf9b2997f4723b7b4f76e61ab58
pfg <pfg@pfg.pw>
2022-01-13 14:16:36 -0500

    fix with changes

--- src/main.zig
+++ src/main.zig
@@ -13,13 +13,13 @@ const smiley = [8]u8{
 
 export fn update() void {
     w4.DRAW_COLORS.* = 0x2;
-    w4.text("Hello from Zig!", 10, 10);
+    w4.text("Hello from Zig!", .{10, 10});
 
     const gamepad = w4.GAMEPAD1.*;
     if (gamepad.button_1) {
         w4.DRAW_COLORS.* = 0x4;
     }
 
-    w4.blit(&smiley, 76, 76, 8, 8, .{ .bpp = .b1 });
-    w4.text("Press X to blink", 16, 90);
+    w4.blit(&smiley, .{76, 76}, .{8, 8}, .{ .bpp = .b1 });
+    w4.text("Press X to blink", .{16, 90});
 }
